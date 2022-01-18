813cfa8182e0dc9fd6c928b3e4cac124e49cf65c
Rano Kuhl <ranokuhl@gmail.com>
2022-01-18 19:53:08 +0100

    add object type products

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const {ApolloServer, gql} = require('apollo-server')
 const typeDefs = gql`
 	type Query {
 		hello: String
+		products: [Product!]!
+	}
+
+	type Product {
+		name: String!
+		description: String!
+		quantity: Int!
+		price: Float!
+		onSale: Boolean!
 	}
 `
 
@@ -10,6 +19,17 @@ const resolvers = {
 	Query: {
 		hello: () => {
 			return 'Hello World!'
+		},
+		products: () => {
+			return [
+				{
+					name: 'Bike',
+					description: 'Mountain Bike',
+					quantity: 3,
+					price: 99.99,
+					onSale: false,
+				},
+			]
 		},
 	},
 }
